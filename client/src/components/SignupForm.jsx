788d72a7afccbf729c56b50e02cd063aa987fbc7
Luis00809 <davidcarvajal008@gmail.com>
2023-11-12 20:37:00 -0800

    sets up calls for queries and mutations

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+
+const [addUser, { error }] = useMutation(ADD_USER);
+
 
 const SignupForm = () => {
   // set initial form state
@@ -28,13 +33,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      
+      const { data } = await addUser({ variables: userFormData });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { token, user } = data.createUser;
 
-      const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
     } catch (err) {
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+
 import {
   Container,
   Col,
@@ -9,7 +11,7 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { SAVE_BOOK } from '../utils/mutations';
 
@@ -65,6 +67,8 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
+    const [saveBook, { error }] = useMutation(SAVE_BOOK)
+
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -73,8 +77,12 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
+      // const response = await saveBook(bookToSave, token);
+      const { response } = await saveBook({
+        variables: {
+          data
+        },
+      })
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
